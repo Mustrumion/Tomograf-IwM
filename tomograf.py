d2503056fa388c439919b629fc1cb4424d302eba
Okonos <tepyzboj@gmail.com>
2016-04-20 19:09:09 +0200

    Poprawki estetyczne

--- tomograf.py
+++ tomograf.py
@@ -19,27 +19,26 @@ def recCountPixelSum(startx, starty, angle, photo): #works, looks like shit thou
     tan = abs( math.tan(angle) )
     if angle < 0.5*math.pi:
 
-        if (startx>len(photo[0])-1 or starty<0):
+        if startx > len(photo[0])-1 or starty < 0:
             return 0
 
-        xleft=(startx+0.5)%1
-        if xleft==0: xleft=1
-        yleft=1-(starty+0.5)%1
+        xleft = (startx+0.5) % 1
+        if xleft == 0: xleft = 1
+        yleft = 1-(starty+0.5) % 1
 
-        xleftscaled = xleft*tan
+        xleftscaled = xleft * tan
 
-        if (xleftscaled>yleft): #top side is crossed first
-
-            newstarty=starty-yleft
-            xleft = yleft/tan
-            newstartx=startx+xleft
+        if (xleftscaled > yleft): #top side is crossed first
+            newstarty = starty - yleft
+            xleft = yleft / tan
+            newstartx = startx + xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.ceil(starty-0.5),math.floor(startx+0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
         else:
-            yleft = xleft*tan
-            newstarty=starty - yleft
-            newstartx=startx + xleft
+            yleft = xleft * tan
+            newstarty = starty - yleft
+            newstartx = startx + xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.ceil(starty-0.5),math.floor(startx+0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
@@ -52,81 +51,77 @@ def recCountPixelSum(startx, starty, angle, photo): #works, looks like shit thou
         return 0
 
     if angle > 0.5*math.pi and angle <= math.pi:
-
-        if (startx<0 or starty<0):
+        if (startx < 0 or starty < 0):
             return 0
 
-        xleft=1-(startx+0.5)%1
-        yleft=1-(starty+0.5)%1
+        xleft = 1-(startx+0.5) % 1
+        yleft = 1-(starty+0.5) % 1
 
-        xleftscaled = xleft*tan
+        xleftscaled = xleft * tan
 
-        if (xleftscaled>yleft): #top side is crossed first
-            xleft = yleft/tan
-            newstarty=starty-yleft
-            newstartx=startx-xleft
+        if (xleftscaled > yleft): #top side is crossed first
+            xleft = yleft / tan
+            newstarty = starty - yleft
+            newstartx = startx - xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.ceil(starty-0.5),math.ceil(startx-0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
         else:
-            yleft = xleft*tan
-            newstarty=starty - yleft
-            newstartx=startx - xleft
+            yleft = xleft * tan
+            newstarty = starty - yleft
+            newstartx = startx - xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.ceil(starty-0.5),math.ceil(startx-0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
 
     if angle > math.pi and angle < 1.5 * math.pi:
-
-        if (startx<0 or starty>len(photo)-1):
+        if (startx < 0 or starty > len(photo)-1):
             return 0
 
-        yleft=(starty+0.5)%1
-        if yleft==0: yleft=1
-        xleft=1-(startx+0.5)%1
+        yleft = (starty+0.5) % 1
+        if yleft == 0: yleft = 1
+        xleft = 1-(startx+0.5) % 1
 
-        xleftscaled = xleft*tan
+        xleftscaled = xleft * tan
 
-        if (xleftscaled>yleft): #top side is crossed first
-
-            newstarty=starty+yleft
-            xleft = yleft/tan
-            newstartx=startx-xleft
+        if (xleftscaled > yleft): #top side is crossed first
+            newstarty = starty + yleft
+            xleft = yleft / tan
+            newstartx = startx - xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.floor(starty+0.5),math.ceil(startx-0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
         else:
-            yleft = xleft*tan
-            newstarty=starty + yleft
-            newstartx=startx - xleft
+            yleft = xleft * tan
+            newstarty = starty + yleft
+            newstartx = startx - xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.floor(starty+0.5),math.ceil(startx-0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
 
     if angle > math.pi * 1.5:
 
-        if (startx>len(photo[0])-1 or starty>len(photo)-1):
+        if (startx > len(photo[0])-1 or starty > len(photo)-1):
             return 0
 
-        yleft=(starty+0.5)%1
-        if yleft==0: yleft=1
-        xleft=(startx+0.5)%1
-        if xleft==0: xleft=1
+        yleft = (starty+0.5) % 1
+        if yleft == 0: yleft = 1
+        xleft = (startx+0.5) % 1
+        if xleft == 0: xleft = 1
 
-        xleftscaled = xleft*tan
+        xleftscaled = xleft * tan
 
-        if (xleftscaled>yleft): #top side is crossed first
-
-            newstarty=starty+yleft
-            xleft = yleft/tan
-            newstartx=startx+xleft
+        if xleftscaled > yleft: #top side is crossed first
+            newstarty = starty + yleft
+            xleft = yleft / tan
+            newstartx = startx + xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.floor(starty+0.5),math.floor(startx+0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
         else:
-            yleft = xleft*tan
-            newstarty=starty + yleft
-            newstartx=startx + xleft
+            yleft = xleft * tan
+            newstarty = starty + yleft
+            newstartx = startx + xleft
             leng = (xleft**2 + yleft**2)**0.5
             return photo[math.floor(starty+0.5),math.floor(startx+0.5)]*leng\
                     +recCountPixelSum(newstartx, newstarty, angle, photo)
@@ -144,47 +139,46 @@ def main():
     else:
         filename = "images/image.bmp"
 
-    output=np.zeros((npoints,nrays))
+    output = np.zeros((npoints,nrays))
 
-    image=rgb2gray(data.imread(filename))
-    width=len(image[0])
-    height=len(image)
-    radius=math.ceil(((width**2+height**2)**0.5)/2) #obliczanie średnicy za pomocą twierdzenia Pitagorasa
+    image = rgb2gray(data.imread(filename))
+    width = len(image[0])
+    height = len(image)
+    radius = math.ceil(((width**2+height**2)**0.5)/2) #obliczanie średnicy za pomocą twierdzenia Pitagorasa
 
-    newwidth=newheight=2*radius + 2  #ustalenie nowej wielkości obrazu
-    if not width%2==0: #sprawdzenie parzystości pikseli (aby środek obrazu pozostał na środku)
+    newwidth = newheight = 2*radius + 2  #ustalenie nowej wielkości obrazu
+    if not width % 2 == 0: #sprawdzenie parzystości pikseli (aby środek obrazu pozostał na środku)
         newwidth+=1
-    if not height%2==0:
+    if not height % 2 == 0:
         newheight+=1
 
     #tworzenie nowego obrazu i kopiowanie starego do środka
-    newimage=np.zeros((newheight,newwidth))
+    newimage = np.zeros((newheight,newwidth))
     #print width, height
     #print newwidth, newheight
-    xstart=(newwidth-width)/2
-    xend=xstart+width
-    ystart=(newheight-height)/2
-    yend=ystart+height
+    xstart = (newwidth-width) / 2
+    xend = xstart + width
+    ystart = (newheight-height) / 2
+    yend = ystart + height
     #print xstart,xend,ystart,yend
     newimage[ystart:yend,xstart:xend]=image
-
 
     f, (ax1, ax2) = plt.subplots(1, 2)
 
     ax1.imshow(newimage,cmap='Greys_r',interpolation='none')
 
-    cirx=((newheight-1)/2) #środek okręgu
-    ciry=((newwidth-1)/2)
+    cirx = ((newheight-1)/2) #środek okręgu
+    ciry = ((newwidth-1)/2)
     circle1=plt.Circle((ciry,cirx),radius,color='r',fill=False)  #wrzucenie okregu do plota
     ax1.add_artist(circle1)
 
 
-    points=np.zeros((npoints,3)) #x,y,kąt stycznej do osi X+
+    points = np.zeros((npoints,3)) #x,y,kąt stycznej do osi X+
 
     for point in xrange(npoints):
-        points[point,0]=cirx+radius * math.cos(2 * math.pi / npoints * point)
-        points[point,1]=ciry+radius * math.sin(2 * math.pi / npoints * point)
-        points[point,2]=2 * math.pi / npoints * point
+        points[point,0] = cirx+radius * math.cos(2 * math.pi / npoints * point)
+        points[point,1] = ciry+radius * math.sin(2 * math.pi / npoints * point)
+        points[point,2] = 2 * math.pi / npoints * point
 
     #print points
 
@@ -192,12 +186,10 @@ def main():
     ax1.plot(points[0,1], points[0,0], 'bs')
     ax1.plot(points[1,1], points[1,0], 'gs')
 
-
-
-    pointnumber=0
+    pointnumber = 0
     for smth in points:
         for ray in xrange(nrays):
-            angle = smth[2] + deadangle + (math.pi - 2 * deadangle)/nrays*ray
+            angle = smth[2] + deadangle + (math.pi - 2 * deadangle) / nrays*ray
             angle = angle % (2 * math.pi)
             #if ray==2: print angle
             output[pointnumber,ray] = recCountPixelSum(smth[1],smth[0],angle,newimage)
@@ -205,11 +197,10 @@ def main():
 
         pointnumber+=1
 
-
     #print points
 
     maxp = output.max(axis=1)
-    if all(maxp>0):
+    if all(maxp > 0):
         for line in output:
             for pixel in line:
                 pixel/=maxp
