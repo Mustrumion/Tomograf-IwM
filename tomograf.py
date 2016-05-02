94708b1284a50584578c7914920effbfeffa722c
Okonos <tepyzboj@gmail.com>
2016-05-02 15:02:46 +0200

    \'Klasyfikacja\'\\'Obiektywizacja\'

--- tomograf.py
+++ tomograf.py
@@ -9,118 +9,117 @@ import sys
 import utils
 
 
-def main():
-    deadangle = 0.4 * math.pi
-    npoints = 50
-    nrays = 50
-    if len(sys.argv) > 1:
-        filename = "images/" + sys.argv[1]
-    else:
-        filename = "images/image.bmp"
+class Tomograph:
+    def __init__(self):
+        self.deadangle = 0.4 * math.pi
+        self.npoints = 50
+        self.points = np.zeros((self.npoints, 3)) #x, y, kąt stycznej do osi X+
+        self.nrays = 50
+        if len(sys.argv) > 1:
+            self.filename = "images/" + sys.argv[1]
+        else:
+            self.filename = "images/image.bmp"
+        self.cirx, self.ciry = 0, 0
+        self.radius = 0
+        self.extendedImage = []
+        self.spectrum = np.zeros((self.npoints, self.nrays))
+        self.reconstructedImage = []
 
-    output = np.zeros((npoints, nrays))
+    def getLinePixels(self, angle, pointerino, ray):
+        x2 = self.cirx + self.radius * math.cos(angle)
+        y2 = self.ciry + self.radius * math.sin(angle)
+        return utils.bresenham(int(round(pointerino[0])), int(round(pointerino[1])), int(round(x2)), int(round(y2)))
 
-    image = rgb2gray(data.imread(filename))
-    width = len(image[0])
-    height = len(image)
-    radius = math.ceil(((width**2+height**2)**0.5)/2) #obliczanie średnicy za pomocą twierdzenia Pitagorasa
+    def scan(self):
+        for pointNumber, pointerino in enumerate(self.points):
+            for ray in xrange(self.nrays):
+                angle = pointerino[2] + self.deadangle + ((2*math.pi - 2*self.deadangle) * float(ray)/self.nrays)
+                pixels = self.getLinePixels(angle, pointerino, ray)
 
-    newwidth = newheight = 2*radius + 2  #ustalenie nowej wielkości obrazu
-    if not width % 2 == 0: #sprawdzenie parzystości pikseli (aby środek obrazu pozostał na środku)
-        newwidth += 1
-    if not height % 2 == 0:
-        newheight += 1
+                weight = 0.0
+                if math.sin(angle%(math.pi/2)) > math.cos(angle%(math.pi/2)):
+                    weight = 1 / math.sin(angle%(math.pi/2))
+                else:
+                    weight = 1 / math.cos(angle%(math.pi/2))
 
-    #tworzenie nowego obrazu i kopiowanie starego do środka
-    newimage = np.zeros((newheight, newwidth))
-    xstart = (newwidth-width) / 2
-    xend = xstart + width
-    ystart = (newheight-height) / 2
-    yend = ystart + height
-    newimage[ystart:yend, xstart:xend] = image
+                self.spectrum[pointNumber, ray] = sum([self.extendedImage[i] for i in pixels]) * weight
 
-    ax1 = plt.subplot(2, 1, 1)
-    ax1.imshow(newimage, cmap='Greys_r', interpolation='none')
+        maxp = self.spectrum.max(axis=1)
+        i = 0
+        if all(maxp > 0):
+            for pixel in np.nditer(self.spectrum, op_flags=['readwrite']):
+                pixel[...] = pixel / maxp[i]
+                i = (i+1) % self.nrays
 
-    # środek okręgu
-    cirx = (newwidth-1) / 2
-    ciry = (newheight-1) / 2
-    # wrzucenie okregu do plota
-    circle1 = plt.Circle((cirx, ciry), radius, color='r', fill=False)
-    ax1.add_artist(circle1)
+    def reconstruct(self):
+        self.reconstructedImage = np.zeros(self.extendedImage.shape)
 
-    points = np.zeros((npoints, 3)) #x, y, kąt stycznej do osi X+
+        for pointNumber, pointerino in enumerate(self.points):
+            for ray in xrange(self.nrays):
+                sample = self.spectrum[pointNumber, ray]
+                if sample > 0:
+                    angle = pointerino[2] + self.deadangle + ((2*math.pi - 2*self.deadangle) * float(ray)/self.nrays)
+                    pixels = self.getLinePixels(angle, pointerino, ray)
+                    rows, cols = [], []
+                    for p in pixels:
+                        rows.append(p[0])
+                        cols.append(p[1])
 
-    step = 2 * math.pi / npoints
-    for point in xrange(npoints):
-        points[point, 0] = cirx + radius * math.cos(step * point)
-        points[point, 1] = ciry + radius * math.sin(step * point)
-        points[point, 2] = step * point
+                    self.reconstructedImage[rows, cols] += sample
 
-    ax1.plot(points[:, 1], points[:, 0], 'ro')
-    ax1.plot(points[0, 1], points[0, 0], 'bs')
-    ax1.plot(points[1, 1], points[1, 0], 'gs')
+    def simulate(self):
+        image = rgb2gray(data.imread(self.filename))
+        width = len(image[0])
+        height = len(image)
+        self.radius = math.ceil(((width**2+height**2)**0.5)/2) #obliczanie średnicy za pomocą twierdzenia Pitagorasa
 
-    # angleStep = (math.pi - 2 * deadangle) / nrays
-    # for pointNumber, smth in enumerate(points):
-    #     for ray in xrange(nrays):
-    #         angle = smth[2] + deadangle + angleStep * ray
-    #         angle = angle % (2 * math.pi)
-    #         #if ray==2: print angle
-    #         output[pointNumber, ray] = recCountPixelSum(smth[0], smth[1], angle, newimage)
-    #         #print deadangle + (math.pi - 2 * deadangle)/nrays*ray
+        newwidth = newheight = 2*self.radius + 2  #ustalenie nowej wielkości obrazu
+        if not width % 2 == 0: #sprawdzenie parzystości pikseli (aby środek obrazu pozostał na środku)
+            newwidth += 1
+        if not height % 2 == 0:
+            newheight += 1
 
-    # Bresenham
-    for pointNumber, pointerino in enumerate(points):
-        for ray in xrange(nrays):
-            angle = pointerino[2] + deadangle + ((2*math.pi - 2*deadangle) * float(ray)/nrays)
-            # angle = smth[2] + deadangle + (math.pi - 2*deadangle) * float(ray)/nrays
-            # angle = angle % (2 * math.pi)
-            x2 = cirx + radius * math.cos(angle)
-            y2 = ciry + radius * math.sin(angle)
-            pixels = utils.bresenham(int(round(pointerino[0])), int(round(pointerino[1])), int(round(x2)), int(round(y2)))
+        #tworzenie nowego obrazu i kopiowanie starego do środka
+        self.extendedImage = np.zeros((newheight, newwidth))
+        xstart = (newwidth-width) / 2
+        xend = xstart + width
+        ystart = (newheight-height) / 2
+        yend = ystart + height
+        self.extendedImage[ystart:yend, xstart:xend] = image
 
-            weight = 0.0
-            if math.sin(angle%(math.pi/2)) > math.cos(angle%(math.pi/2)):
-                weight = 1 / math.sin(angle%(math.pi/2))
-            else:
-                weight = 1 / math.cos(angle%(math.pi/2))
+        ax1 = plt.subplot(2, 1, 1)
+        ax1.imshow(self.extendedImage, cmap='Greys_r', interpolation='none')
 
-            output[pointNumber, ray] = sum([newimage[i] for i in pixels]) * weight
+        # środek okręgu
+        self.cirx = (newwidth-1) / 2
+        self.ciry = (newheight-1) / 2
+        # wrzucenie okregu do plota
+        circle1 = plt.Circle((self.cirx, self.ciry), self.radius, color='r', fill=False)
+        ax1.add_artist(circle1)
 
-    maxp = output.max(axis=1)
-    i = 0
-    if all(maxp > 0):
-        for pixel in np.nditer(output, op_flags=['readwrite']):
-            pixel[...] = pixel / maxp[i]
-            i = (i+1) % nrays
+        step = 2 * math.pi / self.npoints
+        for point in xrange(self.npoints):
+            self.points[point, 0] = self.cirx + self.radius * math.cos(step * point)
+            self.points[point, 1] = self.ciry + self.radius * math.sin(step * point)
+            self.points[point, 2] = step * point
 
-    ax3 = plt.subplot(2, 2, 3)
-    ax3.imshow(output, cmap='Greys_r', interpolation='none')
+        ax1.plot(self.points[:, 1], self.points[:, 0], 'ro')
+        ax1.plot(self.points[0, 1], self.points[0, 0], 'bs')
+        ax1.plot(self.points[1, 1], self.points[1, 0], 'gs')
 
-    # Rekonstrukcja
-    reconstructedImg = np.zeros((newheight, newwidth))
+        # Skanowanie
+        self.scan()
+        ax3 = plt.subplot(2, 2, 3)
+        ax3.imshow(self.spectrum, cmap='Greys_r', interpolation='none')
 
-    for pointNumber, pointerino in enumerate(points):
-        for ray in xrange(nrays):
-            sample = output[pointNumber, ray]
-            if sample > 0:
-                angle = pointerino[2] + deadangle + ((2*math.pi - 2*deadangle) * float(ray)/nrays)
-                x2 = cirx + radius * math.cos(angle)
-                y2 = ciry + radius * math.sin(angle)
-                pixels = utils.bresenham(int(round(pointerino[0])), int(round(pointerino[1])), int(round(x2)), int(round(y2)))
-                rows, cols = [], []
-                for p in pixels:
-                    rows.append(p[0])
-                    cols.append(p[1])
+        # Rekonstrukcja
+        self.reconstruct()
+        ax4 = plt.subplot(2, 2, 4)
+        ax4.imshow(self.reconstructedImage, cmap='Greys_r', interpolation='none')
 
-                reconstructedImg[rows, cols] += sample
-
-    ax4 = plt.subplot(2, 2, 4)
-    ax4.imshow(reconstructedImg, cmap='Greys_r', interpolation='none')
-
-    plt.show()
+        plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    tomograph = Tomograph()
+    tomograph.simulate()
